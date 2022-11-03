95e2827bb8c001b2d8a5d8e1c1c2c58bd971bc0b
ihValery <ihValery@gmail.com>
2022-11-03 23:56:07 +0300

    Initial Commit

---
+++ 3DParallaxEffectWithCoreMotion/_DParallaxEffectWithCoreMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DParallaxEffectWithCoreMotionApp.swift
+//  3DParallaxEffectWithCoreMotion
+//
+//  Created by Валерий Игнатьев on 3.11.22.
+//
+
+import SwiftUI
+
+@main
+struct _DParallaxEffectWithCoreMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
