7bf8d286d486863ba1b2a7eac38dca27b5d18036
ihValery <ihValery@gmail.com>
2022-11-12 18:14:23 +0300

    up

--- 3DParallaxEffectWithCoreMotion/View/SwipeToNext.swift
+++ 3DParallaxEffectWithCoreMotion/View/SwipeToNext.swift
@@ -13,34 +13,64 @@ struct SwipeToNext: View {
     
     //MARK: Properties
     
-    @State private var location: CGPoint = .init(x: 100, y: 100)
+    @State private var location: CGPoint = .zero
     @State private var fingerLocation: CGPoint?
-
+    @GestureState private var isDragging = false
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 15)
-                .fill(.red)
-                .frame(width: 100, height: 100)
-                .position(location)
-                .gesture(
-                    DragGesture()
-                        .onChanged({ value in
-                            location = value.location
-                            fingerLocation = value.location
-                        })
-                        .onEnded({ _ in
-                            fingerLocation = nil
-                        })
-                )
-        
-            if let fingerLocation = fingerLocation {
-                Circle()
-                    .stroke(.green, lineWidth: 2)
-                    .frame(width: 45, height: 45)
-                    .position(fingerLocation)
+        VStack {
+            Text("\(isDragging.description)")
+            
+            ZStack {
+                Color.green
+                    .gesture(
+                        DragGesture(minimumDistance: 0.1)
+                            .updating($isDragging, body: { value, state, transaction in
+                                state = true
+                                fingerLocation = value.location
+                            })
+                            .onEnded({ _ in
+                                fingerLocation = nil
+                            })
+                    )
+                
+                if let fingerLocation = fingerLocation {
+                    RoundedRectangle(cornerRadius: 50)
+//                    Circle()
+                        .stroke(.red, lineWidth: 2)
+                        .frame(width: 45, height: 45 + location.x)
+                        .position(fingerLocation)
+                }
+
             }
         }
-        .ignoresSafeArea()
+        
+//        ZStack {
+//            RoundedRectangle(cornerRadius: 15)
+//                .fill(.red)
+//                .frame(width: 100, height: 100)
+//                .position(location)
+//
+//            Color.gray.opacity(0.2)
+//                .gesture(
+//                    DragGesture()
+//                        .onChanged({ value in
+//                            location = value.location
+//                            fingerLocation = value.location
+//                        })
+//                        .onEnded({ _ in
+//                            fingerLocation = nil
+//                        })
+//                )
+//
+//            if let fingerLocation = fingerLocation {
+//                Circle()
+//                    .stroke(.green, lineWidth: 2)
+//                    .frame(width: 45, height: 45)
+//                    .position(fingerLocation)
+//            }
+//        }
+//        .ignoresSafeArea()
     }
 }
 
